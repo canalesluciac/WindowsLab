89452f1e6d973f6a3415608324c89d583b432c72
Lucia Canales <canalesluciac@gmail.com>
2022-10-21 12:40:02 -0300

    Solución de error al mostrar el nombre del operador

--- WindowsLab/Program.cs
+++ WindowsLab/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsLab
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm1_NotasVectores());
+            Application.Run(new frm3_operarios());
         }
     }
 }
--- WindowsLab/frm3_operarios.cs
+++ WindowsLab/frm3_operarios.cs
@@ -25,28 +25,28 @@ namespace WindowsLab
             double[] sueldo = new double[3];
 
             double totalPagado = 0, suma, acumOperario = 0;
-            string nombreOperario = "nombre";
+            string nombreOperario = "";
 
 
             for (int i = 0; i < nombres.Length; i++)    // operario
             {
                 nombres[i] = cargaNombre(nombres, i);
                 suma = 0;
-                for (int j=0; j<sueldo.Length; j++)     // sueldo mensual
+
+                for (int j = 0; j < sueldo.Length; j++)     // sueldo mensual
                 {
                     sueldo[j] = cargaSueldo(sueldo, j, nombres[i]);
                     suma += sueldo[j];
                 }
-                acumOperario = maximoOperario(suma, acumOperario, i);
-                nombreOperario = maximoOperario(nombres, i, suma, acumOperario);
-
+                
+                acumOperario = maximoOperario(suma, acumOperario, nombres, ref nombreOperario, i) ? suma : acumOperario;
                 totalPagado += suma;
             }
             MessageBox.Show("Total pagado: $" + totalPagado + "\n" + nombreOperario + " tuvo el mayor ingreso acumulado ($" + acumOperario + ")", "Resultados");
         }
 
         #region mis métodos
-        string cargaNombre (string[] nombres, int i)
+        string cargaNombre(string[] nombres, int i)
         {
             return Interaction.InputBox("Nombre del operario: ", "Ingrese nombre");
         }
@@ -54,27 +54,21 @@ namespace WindowsLab
         double cargaSueldo(double[] sueldo, int mes, string nombre)
         {
             string texto = "";
-            texto = "Sueldo de ultimo " + (mes + 1) + "° mes de " + nombre +": ";
+            texto = "Sueldo de ultimo " + (mes + 1) + "° mes de " + nombre + ": ";
             return Convert.ToDouble(Interaction.InputBox(texto, "Ingrese sueldo"));
         }
 
-        string maximoOperario(string[] nombres, int i, double suma, double maximo)
+        bool maximoOperario(double suma, double maximo, string[] nombres, ref string operario, int i = 0)
         {
-            if (i == 0 || suma > maximo)
-                return nombres[0];
+            if (suma > maximo)
+            {
+                operario = nombres[i]; 
+                return true;
+            }
             else
-                return nombres[i];
-        }
+                return false;
+        } 
 
-        double maximoOperario(double suma, double maximo, int i)
-        {
-            if (i == 0 || suma > maximo)
-                return suma;
-            else
-                return -1;
-        }
-
-       
         #endregion
     }
 }
