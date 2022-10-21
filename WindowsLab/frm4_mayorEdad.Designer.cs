b75ca085427be7bcd46b11263d320aba7932de44
Lucia Canales <canalesluciac@gmail.com>
2022-10-21 00:44:28 -0300

    Terminado punto 4

---
+++ WindowsLab/frm4_mayorEdad.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsLab
+{
+    partial class frm4_mayorEdad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(349, 182);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(93, 39);
+            this.btnIngresar.TabIndex = 0;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // frm4_mayorEdad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIngresar);
+            this.Name = "frm4_mayorEdad";
+            this.Text = "frm4_mayorEdad";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+    }
+}
---
+++ WindowsLab/frm4_mayorEdad.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsLab
+{
+    public partial class frm4_mayorEdad : Form
+    {
+        public frm4_mayorEdad()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            const int cantidad = 5;
+            string[] nombres = new string[cantidad];
+            int[] edades = new int[cantidad];
+
+            cargaNombres(cantidad, ref nombres);
+            cargaEdades(ref edades, nombres, cantidad);
+            imprimir(nombres, edades, cantidad);
+        }
+
+    #region mis metodos
+        void cargaNombres(int j, ref string[] nombres)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                nombres[i] = Interaction.InputBox("Ingrese NOMBRE");       
+            }
+        }
+
+        void cargaEdades(ref int[] edades, string[] nombres, int j)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                string aux = "Ingrese EDAD de " + nombres[i];
+                edades[i] = Convert.ToInt32(Interaction.InputBox(aux));
+            }
+        }
+
+        void imprimir(string[] nombres, int[] edades, int cantidad)
+        {
+            string mayoresEdad = "";
+            for (int i=0; i<cantidad; i++)
+            {
+                if (edades[i] >= 18)
+                {
+                    mayoresEdad += " " + nombres[i];
+                }
+            }
+            MessageBox.Show("Personas mayores de edad:" + mayoresEdad);
+        }
+
+        #endregion
+
+    }
+}
